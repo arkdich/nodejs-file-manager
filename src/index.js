dd2cc1deed1a0cc65a49c5ff463b5015f1774a79
Artemy Dyachuk <diachuk.a@mybuh.kz>
2024-01-30 18:28:17 +0600

    add: implement navigation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import { parseArgs } from './lib/parse-args.js'
 import path from 'node:path'
 import fs from 'node:fs/promises'
+import os from 'node:os'
+import { parseArgs } from './lib/parse-args.js'
 import { shutdown } from './lib/utils/shutdown.js'
 import { printWorkingDirectory } from './lib/utils/print-utils.js'
-import os from 'node:os'
+import {
+  changeDirectory,
+  goUpInFileSystem,
+  listDirectory,
+} from './lib/navigation.js'
 
 global.inputArgs = parseArgs(process.argv)
 process.chdir(os.homedir())
@@ -16,15 +21,31 @@ if (global.inputArgs.username) {
 
 printWorkingDirectory()
 
-process.stdin.setEncoding('utf-8').on('data', (input) => {
-  const parsedInput = input.trim()
+process.stdin.setEncoding('utf-8').on('data', async (input) => {
+  const parsedInput = input.trim().split(' ')
 
-  switch (parsedInput) {
+  const command = parsedInput[0]
+  const args = parsedInput.slice(1)
+
+  switch (command) {
     case '.exit':
       shutdown()
-      printWorkingDirectory()
-      return
+      break
+    case 'up':
+      goUpInFileSystem()
+      break
+    case 'cd':
+      changeDirectory(args[0])
+      break
+    case 'ls':
+      await listDirectory(args[0])
+      break
+    default:
+      process.stdout.write('Invalid input\n')
+      break
   }
+
+  printWorkingDirectory()
 })
 
 process.on('SIGINT', shutdown)
