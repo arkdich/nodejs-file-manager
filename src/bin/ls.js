fc4ec90bedf4348fd90533a3cfa1ea93cd491704
Artemy Dyachuk <artem002498@gmail.com>
2024-02-04 12:42:49 +0600

    add: sort ls output

--- src/bin/ls.js
+++ src/bin/ls.js
@@ -3,6 +3,12 @@ import { truncateString } from '../lib/utils.js'
 
 const dirContents = await readdir('.', { withFileTypes: true })
 
+dirContents.sort((a, b) => {
+  if (a.isDirectory() && b.isFile()) return -1
+
+  return a.name.localeCompare(b.name)
+})
+
 const dirInfo = dirContents.map((dir) => ({
   Name: truncateString(dir.name),
   Type: dir.isFile() ? 'file' : 'directory',
